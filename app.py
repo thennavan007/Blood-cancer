4d3d74458042e60af4cf496b82509f298c1e3307
thennavan007 <126567851+thennavan007@users.noreply.github.com>
2024-05-07 11:49:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def prediction_page():
     from tensorflow.keras.models import load_model
 
     # Load the trained model
-    model = load_model('Nasnet (1).h5')
+    model = load_model('Nasnet(1).h5')
 
     # Define class labels
     class_labels = {0: 'Early-pre-B', 1: 'Pre-B', 2: 'Pro-B', 3: 'Benign'}
